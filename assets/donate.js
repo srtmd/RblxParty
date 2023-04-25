cb11539e16e23a339be844f6bc8826cfb18f85f9
srtmd <46230056+srtmd@users.noreply.github.com>
2023-04-24 22:49:07 -0300

    1

--- assets/donate.js
+++ assets/donate.js
@@ -4,5 +4,21 @@ const param = window.location.search;
 const urlpa = new URLSearchParams(param);
 if (urlpa.has('donate')) {
 	const usr = urlpa.get('donate');
+	document.title = usr + " need you!";
+	document.description = "Donate Robux to " + usr + " in Pls Donate!";
+	document.querySelectorAll('meta[property=og\\:image]')[0].setAttribute('content', 'http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=' + usr);
 	window.location.replace(link1 + usr + link2);
+}
+
+function getUserID()
+{
+	const user = urlpa.get('donate');
+    fetch(`https://www.roblox.com/users/profile?username=${user}`)
+        .then(r => {
+            if (!r.ok) { throw "Invalid response"; }
+            return r.url.match(/\d+/)[0];
+        })
+        .then(id => {
+            console.log(id);
+        })
 }
